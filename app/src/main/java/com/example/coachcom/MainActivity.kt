bfebbc9d75220d2e7a125abe47960f8b450bda52
Robert Gladoić <rgladoic@foi.hr>
2023-11-14 11:36:19 +0100

    Added homepage fragments

--- app/src/main/java/com/example/coachcom/MainActivity.kt
+++ app/src/main/java/com/example/coachcom/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.coachcom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.coachcom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         //setContentView(R.layout.activity_main)
-        setContentView(R.layout.login)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.messages -> replaceFragment(MessagesFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.workouts -> replaceFragment(WorkoutsFragment())
+                R.id.settings_title -> replaceFragment(SettingsFragment())
+            }
+            true
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
+
